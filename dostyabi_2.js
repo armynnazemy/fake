1e45cd587e81c898dda2e7e499fabb4fd4700ecd
armynnazemi <armin4030nazemi@gmail.com>
2023-06-19 09:00:44 -0700

    girls_2

--- dostyabi_2.js
+++ dostyabi_2.js
@@ -21,4 +21,6 @@ for(let i=1;i<girls_2.length;i++){
     console.log(girls_2[i])
     girls_2[i].age = girls_2[i].age + 2
     console.log(girls_2[i])
+    girls_2[i].skin= "black"
+    console.log(girls_2[i])
 }
