7806159fe9e293d112c5aee5a8ba846d68d92106
armynnazemi <armin4030nazemi@gmail.com>
2023-06-21 12:23:26 -0700

    loop

--- loop.js
+++ loop.js
@@ -1,3 +1,32 @@
 for(let haha = 8; haha > 2;haha--) {
     console.log(haha)
 }
+
+
+//while loop
+let I = 0
+
+let  girls = [
+ { 
+     name : "shahla",
+     age   : 48
+  },
+{
+
+    name : "sara",
+    age : 18
+}
+]
+
+
+while(I < 10) {
+
+ if(girls[I].age < 25) {
+
+   console.log(I)
+   I = I + 1
+  }else {
+   break
+}
+
+}
